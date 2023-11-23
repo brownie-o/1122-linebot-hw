f8a35b60ef5fa82ff79af67857d0231fea63585b
brownie-o <amyooxx1024@gmail.com>
2023-11-24 00:28:39 +0800

    想辦法將取到的資料作為id被搜尋(?

--- commands/villagers.js
+++ commands/villagers.js
@@ -3,8 +3,10 @@ import * as cheerio from 'cheerio'
 import villagersTemplate from '../templates/villagers.js'
 import fs from 'node:fs'
 
-export default async (event) => {
+const main = async (event) => {
   try {
+    const id = event.message.text()
+    // id 會是 名字，性格，生日(月份?)，種類(Kind)
     const { data } = await axios.post('https://animalcrossing.soopoolleaf.com/tw/acnh/animalsearch/load_data.php', {
       // Payload
       Category: '',
@@ -38,31 +40,72 @@ export default async (event) => {
       }
     })
 
-    console.log(data)
-    const $ = cheerio.load(data)
+    for (const villager of data.data) {
+      let $ = cheerio.load(villager.名字)
+      if (id == $('a').text()) {
+        const 名字 = $('a').text()
+      }
+    }
 
-    const template = villagersTemplate()
-    template.hero.url = $()
+    for (const villager of data.data) {
+      // console.log(villager.性格)
+      let $ = cheerio.load(villager.名字)
+      const 名字 = $('a').text()
+      $ = cheerio.load(villager.性格)
+      const 性格 = $('a').text()
+      $ = cheerio.load(villager.性别)
+      const 性別 = $('span').text()
+      $ = cheerio.load(villager.生日)
+      const 生日 = $('a').text()
+      // 有些沒口頭禪的npc 就要用 || ''
+      $ = cheerio.load(villager.初始口頭禪 || '')
+      const 口頭禪 = $('span').text()
+      $ = cheerio.load(villager.樣式 || '')
+      const 樣式 = $('span').text()
+      $ = cheerio.load(villager.樣式2 || '')
+      const 樣式2 = $('span').text()
+      const 樣式3 = `${樣式}/${樣式2}`
+      $ = cheerio.load(villager.顏色 || '')
+      const 顏色 = $('span').text()
+      $ = cheerio.load(villager.顏色2 || '')
+      const 顏色2 = $('span').text()
+      const 顏色3 = `${顏色}/${顏色2}`
+      const Img = villager.Img
+      $ = cheerio.load(villager.Kind)
+      const 種類 = $('a').text()
+      console.log(種類)
+
+      const template = villagersTemplate()
+      template.hero.url = Img
+      template.body.contents[0].text = 名字
+      template.body.contents[1].contents[0].contents[1].text = 性格
+      template.body.contents[1].contents[1].contents[1].text = 口頭禪
+      template.body.contents[1].contents[2].contents[1].text = 性別
+      template.body.contents[1].contents[3].contents[1].text = 生日
+      template.body.contents[1].contents[6].contents[1].text = 顏色3
+      template.body.contents[1].contents[7].contents[1].text = 樣式3
+
+      if (process.env.DEBUG === 'true') {
+        fs.writeFileSync('./dump/villagers.json', JSON.stringify(template, null, 2))
+      }
+
+      // function replacer (key, value) {
+      //   if (key === '性格') {
+      //     if (value === '运动') {
+      //       return '運動'
+      //     }
+      //     if (value === '悠闲') {
+      //       return '悠閒'
+      //     }
+      //     if (value === '元气') {
+      //       return '元氣'
+      //     }
+      //   }
+      // }
+    }
   } catch (error) {
     console.log(error)
   }
 }
 
-if (process.env.DEBUG === 'true') {
-  const template = villagersTemplate()
-  fs.writeFileSync('./dump./villagers.json', JSON.stringify(template, replacer, 2))
-}
-
-function replacer (key, value) {
-  if (key === '性格') {
-    if (value === '运动') {
-      return '運動'
-    }
-    if (value === '悠闲') {
-      return '悠閒'
-    }
-    if (value === '元气') {
-      return '元氣'
-    }
-  }
-}
+main()
--- index.js
+++ index.js
@@ -12,4 +12,12 @@ bot.on('message', event => {
   if (process.env.DEBUG === 'true') {
     console.log(event)
   }
+
+  if (event.message.type === 'text') {
+    villagers(event)
+  }
+})
+
+bot.listen('/', process.env.PORT || 3000, () => {
+  console.log('機器人啟動')
 })
