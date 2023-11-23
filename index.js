6cd4efcd5d5e5436d8310761641d9689cde14bd6
brownie-o <amyooxx1024@gmail.com>
2023-11-23 11:46:46 +0800

    started editing index

---
+++ index.js
@@ -0,0 +1,15 @@
+import 'dotenv/config'
+import linebot from 'linebot'
+import villagers from './commands/villagers.js'
+
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
+
+bot.on('message', event => {
+  if (process.env.DEBUG === 'true') {
+    console.log(event)
+  }
+})
