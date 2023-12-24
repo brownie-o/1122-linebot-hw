46503135134855da7916ef50a951e0148297d542
brownie-o <amyooxx1024@gmail.com>
2023-12-25 01:47:46 +0800

    成功偵測&發送機器人訊息!!!

--- commands/villagers.js
+++ commands/villagers.js
@@ -1,12 +1,13 @@
 import axios from 'axios'
 import * as cheerio from 'cheerio'
 import villagersTemplate from '../templates/villagers.js'
-import fs from 'node:fs'
+// import fs from 'node:fs'
 
-const main = async (event) => {
+export default async (event) => {
   try {
-    const id = event.message.text()
+    const id = event.message.text.trim()
     // id 會是 名字，性格，生日(月份?)，種類(Kind)
+    // 要驗證嗎...?
     const { data } = await axios.post('https://animalcrossing.soopoolleaf.com/tw/acnh/animalsearch/load_data.php', {
       // Payload
       Category: '',
@@ -44,68 +45,67 @@ const main = async (event) => {
       let $ = cheerio.load(villager.名字)
       if (id === $('a').text()) {
         const 名字 = $('a').text()
+        $ = cheerio.load(villager.性格)
+        let 性格 = $('a').text()
+        if (性格 === '运动') {
+          性格 = '運動'
+        } else if (性格 === '悠闲') {
+          性格 = '悠閒'
+        } else if (性格 === '元气') {
+          性格 = '元氣'
+        } else if (性格 === '自恋') {
+          性格 = '自戀'
+        }
+        $ = cheerio.load(villager.性别)
+        let 性別 = $('span').text()
+        if ($('span').text() === '♂♂') {
+          性別 = '雄'
+        } else 性別 = '母'
+        $ = cheerio.load(villager.生日)
+        const 生日 = $('a').text()
+        // 有些沒口頭禪的npc 就要用 || ''
+        $ = cheerio.load(villager.初始口頭禪 || '')
+        const 口頭禪 = $('span').text() || '無'
+        $ = cheerio.load(villager.樣式 || '')
+        const 樣式 = $('span').text()
+        $ = cheerio.load(villager.樣式2 || '')
+        const 樣式2 = $('span').text()
+        const 樣式3 = `${樣式}/${樣式2}`
+        $ = cheerio.load(villager.顏色 || '')
+        let 顏色 = $('span').text()
+        $ = cheerio.load(villager.顏色2 || '')
+        let 顏色2 = $('span').text()
+        const 顏色3 = `${顏色 = (顏色 === '紅') ? '紅色' : 顏色}/${顏色2 = (顏色2 === '紅') ? '紅色' : 顏色2}`
+        const Img = villager.Img
+        $ = cheerio.load(villager.Kind)
+        const 種類 = $('a').text()
+        console.log(種類)
+
+        const template = villagersTemplate()
+        template.hero.url = Img
+        template.body.contents[0].text = 名字
+        template.body.contents[1].contents[0].contents[0].contents[1].text = 性格
+        template.body.contents[1].contents[0].contents[1].contents[1].text = `"${口頭禪}"`
+        template.body.contents[1].contents[1].contents[0].contents[1].text = 性別
+        template.body.contents[1].contents[1].contents[1].contents[1].text = 生日
+        template.body.contents[1].contents[4].contents[1].text = 顏色3
+        template.body.contents[1].contents[5].contents[1].text = 樣式3
+
+        const result = await event.reply({
+          type: 'flex',
+          altText: `${名字}的資訊`,
+          contents: template
+        })
+        console.log(result)
       }
     }
 
-    for (const villager of data.data) {
-      // console.log(villager.性格)
-      let $ = cheerio.load(villager.名字)
-      const 名字 = $('a').text()
-      $ = cheerio.load(villager.性格)
-      const 性格 = $('a').text()
-      $ = cheerio.load(villager.性别)
-      const 性別 = $('span').text()
-      $ = cheerio.load(villager.生日)
-      const 生日 = $('a').text()
-      // 有些沒口頭禪的npc 就要用 || ''
-      $ = cheerio.load(villager.初始口頭禪 || '')
-      const 口頭禪 = $('span').text()
-      $ = cheerio.load(villager.樣式 || '')
-      const 樣式 = $('span').text()
-      $ = cheerio.load(villager.樣式2 || '')
-      const 樣式2 = $('span').text()
-      const 樣式3 = `${樣式}/${樣式2}`
-      $ = cheerio.load(villager.顏色 || '')
-      const 顏色 = $('span').text()
-      $ = cheerio.load(villager.顏色2 || '')
-      const 顏色2 = $('span').text()
-      const 顏色3 = `${顏色}/${顏色2}`
-      const Img = villager.Img
-      $ = cheerio.load(villager.Kind)
-      const 種類 = $('a').text()
-      console.log(種類)
-
-      const template = villagersTemplate()
-      template.hero.url = Img
-      template.body.contents[0].text = 名字
-      template.body.contents[1].contents[0].contents[1].text = 性格
-      template.body.contents[1].contents[1].contents[1].text = 口頭禪
-      template.body.contents[1].contents[2].contents[1].text = 性別
-      template.body.contents[1].contents[3].contents[1].text = 生日
-      template.body.contents[1].contents[6].contents[1].text = 顏色3
-      template.body.contents[1].contents[7].contents[1].text = 樣式3
-
-      if (process.env.DEBUG === 'true') {
-        fs.writeFileSync('./dump/villagers.json', JSON.stringify(template, null, 2))
-      }
-
-      // function replacer (key, value) {
-      //   if (key === '性格') {
-      //     if (value === '运动') {
-      //       return '運動'
-      //     }
-      //     if (value === '悠闲') {
-      //       return '悠閒'
-      //     }
-      //     if (value === '元气') {
-      //       return '元氣'
-      //     }
-      //   }
-      // }
-    }
+    // if (process.env.DEBUG === 'true') {
+    //   fs.writeFileSync('./dump/villagers.json', JSON.stringify(template, null, 2))
+    // }
   } catch (error) {
     console.log(error)
   }
 }
 
-main()
+// main()
