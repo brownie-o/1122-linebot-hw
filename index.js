4f5fdb3b7fe8cadd42fcc85154387de7b55e7191
brownie-o <amyooxx1024@gmail.com>
2023-12-25 22:21:46 +0800

    added bubble

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import 'dotenv/config'
 import linebot from 'linebot'
 import villagers from './commands/villagers.js'
-// import kind
+// import kind from './commands/kind.js'
+import aquaticAnimals from './commands/aquaticAnimals.js'
 
 const bot = linebot({
   channelId: process.env.CHANNEL_ID,
@@ -15,11 +16,109 @@ bot.on('message', event => {
     console.log(event)
   }
 
-  const animal = ['鴨子', '食蟻獸', '狗', '兔子', '馬', '公牛', '青蛙', '狼', '河馬', '袋鼠', '熊', '無尾熊', '小熊', '猩猩', '犀牛', '猴子', '小鹿', '大象', '章魚', '鴕鳥', '老虎', '鳥', '雞', '貓', '老鼠', '倉鼠', '綿羊', '豬', '企鵝', '母牛', '山羊', '獅子', '松鼠', '鵰', '鱷魚']
+  // const animal = ['鴨子', '食蟻獸', '狗', '兔子', '馬', '公牛', '青蛙', '狼', '河馬', '袋鼠', '熊', '無尾熊', '小熊', '猩猩', '犀牛', '猴子', '小鹿', '大象', '章魚', '鴕鳥', '老虎', '鳥', '雞', '貓', '老鼠', '倉鼠', '綿羊', '豬', '企鵝', '母牛', '山羊', '獅子', '松鼠', '鵰', '鱷魚']
 
   if (event.message.type === 'text') {
-    if (animal.includes(event.message.text)) {
-      kind(event)
+    if (event.message.text.includes('種族')) {
+      event.reply({
+        type: 'text',
+        text: '正在以種族查詢',
+        quickReply: {
+          items: [
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                text: '水生動物',
+                label: '水生動物'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                text: '小型陸上動物',
+                label: '小型陸上動物'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                text: '大型陸上動物',
+                label: '大型陸上動物'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                text: '飛行動物',
+                label: '飛行動物'
+              }
+            }
+          ]
+        }
+      })
+    } else if (event.message.text.trim() === '水生動物') {
+      aquaticAnimals(event)
+
+      event.reply({
+        type: 'text',
+        text: '正在查詢水生動物',
+        quickReply: {
+          items: [
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                text: '鴨子',
+                label: '鴨子'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                text: '青蛙',
+                label: '青蛙'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                text: '章魚',
+                label: '章魚'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                text: '河馬',
+                label: '河馬'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                text: '企鵝',
+                label: '企鵝'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'message',
+                text: '鳄鱼',
+                label: '鳄鱼'
+              }
+            }
+          ]
+        }
+      })
     } else {
       villagers(event)
     }
