7b6a8bb1005b18934eaed6609245677d27f2b7f5
brownie-o <amyooxx1024@gmail.com>
2023-12-26 01:15:10 +0800

    將12隻以上的動物分類，多加一層bubble

--- commands/kind_for.js
+++ commands/kind_for.js
@@ -1,42 +1,8 @@
-// import axios from 'axios'
 import * as cheerio from 'cheerio'
 import villagersTemplate from '../templates/villagers.js'
 
 export default async (data, id, replies) => {
   try {
-    // const { data } = await axios.post('https://animalcrossing.soopoolleaf.com/tw/acnh/animalsearch/load_data.php', {
-    //   // Payload
-    //   Category: '',
-    //   Category_Per: '',
-    //   Category_B: '',
-    //   Hobby: '',
-    //   Sub_type: '',
-    //   Greetings_type: '',
-    //   Sociability: '',
-    //   BGM: '',
-    //   toptop: '',
-    //   Umbrella: '',
-    //   Style_1: '',
-    //   Style_2: '',
-    //   Color_1: '',
-    //   Color_2: '',
-    //   Wallpaper: '',
-    //   Flooring: '',
-    //   Search: '',
-    //   Initial_phrase: '',
-    //   Favorite_saying: '',
-    //   OnlyNew: false,
-    //   page: 1,
-    //   pageLimit: 500
-    // }, {
-    //   headers: {
-    //     // indicate the media type of the resource
-    //     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    //     // 騙對方發請求的是瀏覽器
-    //     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
-    //   }
-    // })
-
     for (const villager of data.data) {
       let $ = cheerio.load(villager.Kind)
       if (id === $('a').text()) {
